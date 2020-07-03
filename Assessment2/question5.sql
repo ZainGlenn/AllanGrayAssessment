19c854a04a2c727063af0148b6a3e155668bb6ba
Zain Glenn <34332558+ZainGlenn@users.noreply.github.com>
2020-07-03 21:28:36 +0200

    Update question5.sql
    question 5 answered

--- Assessment2/question5.sql
+++ Assessment2/question5.sql
@@ -1,14 +1,8 @@
-
-#Question 1
-SELECT DISTINCT
-  COUNT(id) AS userCount
-FROM Users;
-
-#Question 2
-SELECT count(id) AS Users,year(CreationDate) AS YearCreated FROM Users 
-  where year(CreationDate) > 2009
-    group by year(CreationDate)
-      order by year(CreationDate) ASC;
+SELECT top 1 b.UserId,b.Name,b.Date,u.DisplayName,u.Location,u.Reputation FROM Badges AS b
+  INNER JOIN Users AS u ON b.UserId = u.id
+    WHERE u.Location = 'South Africa'
+      AND b.Name = 'Enlightened'
+        ORDER BY u.Reputation desc;
 
 
 
