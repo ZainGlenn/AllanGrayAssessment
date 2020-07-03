9b844e2147928b00b19528b6e0788ba84c4bbc85
Zain Glenn <34332558+ZainGlenn@users.noreply.github.com>
2020-07-03 20:09:17 +0200

    updated to question 3's answers

--- Assessment2/question3.sql
+++ Assessment2/question3.sql
@@ -1,15 +1,7 @@
 
-#Question 1
-SELECT DISTINCT
-  COUNT(id) AS userCount
-FROM Users;
-
-#Question 2
-SELECT count(id) AS Users,year(CreationDate) AS YearCreated FROM Users 
-  where year(CreationDate) > 2009
-    group by year(CreationDate)
-      order by year(CreationDate) ASC;
-
-
-
+SELECT count(pt.Name) as QuestionPostCount 
+  FROM Posts AS p 
+    INNER JOIN PostTypes AS pt ON p.PostTypeId = pt.id 
+      WHERE p.OwnerDisplayName = 'Mat Mannion' 
+        AND pt.Name= 'Question';
 
