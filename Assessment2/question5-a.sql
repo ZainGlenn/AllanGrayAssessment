3fc170c0e4846a6373a127bd63b4acca9ffd6a5a
Zain Glenn <34332558+ZainGlenn@users.noreply.github.com>
2020-07-03 22:38:33 +0200

    Update question5-a.sql
    updated correct answer

--- Assessment2/question5-a.sql
+++ Assessment2/question5-a.sql
@@ -1,17 +1,9 @@
-SELECT top 1 b.UserId,b.Name,b.Date,u.DisplayName,u.Location,u.Reputation FROM Badges AS b
-  INNER JOIN Users AS u ON b.UserId = u.id
-    WHERE u.Location = 'South Africa'
-      AND b.Name = 'Enlightened'
-        ORDER BY u.Reputation desc;
-        
-SELECT count(b.Name),u.Reputation,u.DisplayName FROM Badges AS b
+SELECT top 1 count(b.Name),u.Reputation,u.DisplayName FROM Badges AS b
   INNER JOIN Users AS u ON b.UserId = u.id
     WHERE Year(b.Date) = 2016
       AND b.Name = 'Enlightened'
         AND u.Location = 'South Africa'
         group by u.Reputation,u.DisplayName
         order by u.Reputation desc
-
-
 
 
